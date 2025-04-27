90d71cd8f4165e165d016fc9ddfe2b73c2ed792a
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-04-27 16:00:39 -0600

    waos

--- stubs.lua
+++ stubs.lua
@@ -1,4 +1,9 @@
---- loads formats for the explorer
----@function load_formats
 ---@param t table
 load_formats = function (t) end
+
+---@param t table
+---@return string
+set_color  = function (r, g, b) end
+
+---@return string
+reset_col  = function () end
