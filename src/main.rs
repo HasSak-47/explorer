c08f0f4e1ebd7a152e95eda40fe3b20f6b077684
arch machine <lilith.alanis.255@gmail.com>
2025-05-03 17:18:52 -0600

    reformat

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,60 @@
+mod api;
+mod explorer;
+mod fmt;
 mod list;
 mod util;
-mod fmt;
-mod explorer;
-mod api;
 
-use api::{get_formats, bash};
-use explorer::Explorer;
+use api::{bash, get_formats};
 use list::List;
 use util::*;
 
 use std::{
-    collections::HashMap, env::home_dir, fs::File, io::Read, path::PathBuf, process::exit, sync::{LazyLock, Mutex, OnceLock}
+    collections::HashMap,
+    env::home_dir,
+    fs::File,
+    io::Read,
+    path::PathBuf,
+    process::exit,
+    sync::{LazyLock, Mutex, OnceLock},
 };
 
-
-use clap::Parser;
 use anyhow::{anyhow, Result};
+use clap::Parser;
 use mlua::{Function, Lua};
 
-pub static MAP : LazyLock<Mutex< HashMap<FileType, mlua::Function>>> = LazyLock::new(||{ 
+pub static MAP: LazyLock<Mutex<HashMap<FileType, mlua::Function>>> = LazyLock::new(|| {
     use FileType as FT;
     let lua = LUA.lock().unwrap();
 
-    let def_file: Function = lua.load("function(name, path, tick) return '󰈔 ' .. name end").eval().unwrap();
-    let def_dir: Function = lua.load("function(name, path, tick) return ' ' .. name .. '/' end").eval().unwrap();
-    let def_link: Function = lua.load("function(name, path, tick) return ' ' .. name .. '@' end").eval().unwrap();
+    let def_file: Function = lua
+        .load("function(name, path, tick) return '󰈔 ' .. name end")
+        .eval()
+        .unwrap();
+    let def_dir: Function = lua
+        .load("function(name, path, tick) return ' ' .. name .. '/' end")
+        .eval()
+        .unwrap();
+    let def_link: Function = lua
+        .load("function(name, path, tick) return ' ' .. name .. '@' end")
+        .eval()
+        .unwrap();
 
     return Mutex::new(HashMap::from([
         (FT::GenericFile, def_file),
-        (FT::GenericDir , def_dir),
+        (FT::GenericDir, def_dir),
         (FT::GenericSymLink, def_link),
     ]));
 });
 
-pub static LUA: LazyLock<Mutex<Lua>> = LazyLock::new( || Mutex::new(Lua::new()) );
+pub static LUA: LazyLock<Mutex<Lua>> = LazyLock::new(|| Mutex::new(Lua::new()));
 
 #[allow(dead_code)]
 fn config_dir() -> PathBuf {
-    let mut config_dir = match home_dir(){
+    let mut config_dir = match home_dir() {
         Some(s) => s,
         None => {
             exit(-1);
-        },
+        }
     };
 
     config_dir.push(".config");
@@ -52,9 +65,19 @@ fn config_dir() -> PathBuf {
     return config_dir;
 }
 
+#[allow(dead_code)]
+fn curr_dir() -> PathBuf {
+    use std::env::current_dir;
+    return match current_dir() {
+        Ok(s) => s,
+        Err(_) => {
+            exit(-1);
+        }
+    };
+}
 
 #[derive(Parser)]
-enum Mode{
+enum Mode {
     List(List),
     Explorer,
 }
@@ -65,10 +88,13 @@ pub struct Options {
     #[arg(long, short, default_value = config_dir().display().to_string())]
     config: PathBuf,
 
-    #[arg(long, short, default_value_t=false)]
+    #[arg(default_value = curr_dir().display().to_string())]
+    path: PathBuf,
+
+    #[arg(long, short, default_value_t = false)]
     debug: bool,
 
-    #[arg(long, short, default_value_t=false)]
+    #[arg(long, short, default_value_t = false)]
     verbose: bool,
 
     #[arg(value_enum, long, short, default_value_t=SortBy::Name)]
@@ -78,14 +104,14 @@ pub struct Options {
     mode: Mode,
 }
 
-static OPTIONS : OnceLock<Options> = OnceLock::new();
+static OPTIONS: OnceLock<Options> = OnceLock::new();
 
-pub fn get_options<'a>() -> &'a Options{
-    OPTIONS.get_or_init( Options::parse )
+pub fn get_options<'a>() -> &'a Options {
+    OPTIONS.get_or_init(Options::parse)
 }
 
 fn init_lua() -> Result<()> {
-    let lua = LUA.lock().map_err(|err| anyhow!(err.to_string()) )?;
+    let lua = LUA.lock().map_err(|err| anyhow!(err.to_string()))?;
     let path = &get_options().config;
 
     let load_format_function = lua.create_function(get_formats)?;
@@ -93,7 +119,7 @@ fn init_lua() -> Result<()> {
     let bash_function = lua.create_function(bash)?;
     lua.globals().set("bash", bash_function)?;
 
-    if let Ok(mut file) = File::open(path){
+    if let Ok(mut file) = File::open(path) {
         let mut buf = String::new();
         file.read_to_string(&mut buf)?;
         lua.load(&buf).exec()?;
@@ -107,13 +133,14 @@ fn setup_lua() {
 }
 
 fn main() -> Result<()> {
-    OPTIONS.get_or_init( Options::parse );
+    println!("\x1b[m");
+    OPTIONS.get_or_init(Options::parse);
 
     setup_lua();
     init_lua()?;
-    match &get_options().mode{
-        Mode::List(ls) => ls.ls()?,//print_data()? ,
-        Mode::Explorer => Explorer::new().render()?,
+    match &get_options().mode {
+        Mode::List(ls) => ls.ls()?, //print_data()? ,
+        Mode::Explorer => {}
     }
 
     Ok(())
--- stubs.lua
+++ stubs.lua
@@ -1,6 +1,8 @@
 ---@param t table
-load_formats = function (t) end
+load_formats = function(t) end
 
 ---@param s string
----@return string
-bash = function (s) return "" end
+---@return (string, string, number)
+bash = function(s)
+	return ""
+end
